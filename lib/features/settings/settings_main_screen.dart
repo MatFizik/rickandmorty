6b58b330105d2d278f2d0712801d8e1cb9c014a8
001538 <aniyazov@halykbank.kg>
2025-04-01 21:24:57 +0600

    переключатель темы

--- lib/features/settings/settings_main_screen.dart
+++ lib/features/settings/settings_main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rickandmorty/common/theme/bloc/cubit/theme_cubit.dart';
+import 'package:rickandmorty/common/widgets/tiles/custom_mini_tile_widget.dart';
 
 class SettingsMainScreen extends StatefulWidget {
   const SettingsMainScreen({super.key});
@@ -10,8 +13,22 @@ class SettingsMainScreen extends StatefulWidget {
 class _SettingsMainScreenState extends State<SettingsMainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Settings'),
+    bool isDarkTheme =
+        context.watch<ThemeCubit>().state.brightness == Brightness.dark;
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          height: 50,
+          child: CustomMiniTileWidget(
+            title: 'Тема оформления',
+            subtitle: isDarkTheme ? 'Темная тема' : 'Светлая тема',
+            onTap: () {
+              context.read<ThemeCubit>().toggleTheme(isDarkTheme);
+            },
+          ),
+        ),
+      ),
     );
   }
 }
